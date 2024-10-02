3845296537162f5429b2f4ba1a43573396dbf75c
Erick Cuevas <34387169+Erickcufe@users.noreply.github.com>
2024-10-01 20:00:49 -0600

    Update test.py
    .

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ def division(a, b):
 
 def test_division():
     assert division(4, 2) == 2
-    assert division(0, 1) == 0
 
 def test_division_by_zero():
     with pytest.raises(ZeroDivisionError):
